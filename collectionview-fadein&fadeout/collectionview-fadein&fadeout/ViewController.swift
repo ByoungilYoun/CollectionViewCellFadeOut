8f34fc1fe717ad179aec856d6bf076757fecb4de
YounByoungil <jimmy7525@naver.com>
2021-07-29 15:57:10 +0900

    Setup two ImageViews

--- collectionview-fadein&fadeout/collectionview-fadein&fadeout/ViewController.swift
+++ collectionview-fadein&fadeout/collectionview-fadein&fadeout/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+  //MARK: - Properties
+  
+  private let myImageView1 : UIImageView = {
+    let v = UIImageView()
+    v.image = UIImage(named: "JoopdaClothes")
+    return v
+  }()
+  
+  private let myImageView2 : UIImageView = {
+    let v = UIImageView()
+    v.image = UIImage(named: "JoopdaCollaboration")
+    return v
+  }()
+  //MARK: - Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    configureUI()
+    setupView()
   }
 
-
+  //MARK: - Functions
+  private func configureUI() {
+    view.backgroundColor = .lightGray
+    
+    [myImageView1, myImageView2].forEach {
+      view.addSubview($0)
+    }
+    
+    myImageView1.snp.makeConstraints {
+      $0.top.leading.trailing.bottom.equalToSuperview()
+    }
+    
+    myImageView2.snp.makeConstraints {
+      $0.top.leading.trailing.bottom.equalToSuperview()
+    }
+  }
+  
+  private func setupView() {
+    myImageView1.alpha = 1.0
+    myImageView2.alpha = 0.0
+    
+  }
 }
 
