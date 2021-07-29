8f093d20dc971bd323fbacf9156eb5305d22a229
YounByoungil <jimmy7525@naver.com>
2021-07-29 15:23:33 +0900

    Set Project

---
+++ collectionview-fadein&fadeout/collectionview-fadein&fadeout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  collectionview-fadein&fadeout
+//
+//  Created by 윤병일 on 2021/07/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ collectionview-fadein&fadeout/collectionview-fadein&fadeoutTests/collectionview_fadein_fadeoutTests.swift
@@ -0,0 +1,33 @@
+//
+//  collectionview_fadein_fadeoutTests.swift
+//  collectionview-fadein&fadeoutTests
+//
+//  Created by 윤병일 on 2021/07/29.
+//
+
+import XCTest
+@testable import collectionview_fadein_fadeout
+
+class collectionview_fadein_fadeoutTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
